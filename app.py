a4a7e77cca2785cae0d4d16cabdb4dd4a35405ee
gt1854 <gt1854@naver.com>
2019-04-12 18:05:04 +0900

    2019.04.12 18:04

--- app.py
+++ app.py
@@ -9,5 +9,23 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/calc')
+def calc():
+    stmt = request.args.get('stmt', 'NONE')
+    if (stmt == 'NONE'):
+        print('넘어온값 없음')
+    else:
+        print('넘어온 식 :: {}'.format(stmt))
+        patt = '[0-9]+'
+        op = re.sub(patt, '', stmt)
+        print('넘어온 연산자 :: {}'.format(op))
+        nums = stmt.split(op)
+
+        if op == '+':
+            result = int(nums[0]) + int(nums[1])
+
+    return jsonify(result=result)
+
+
 if __name__ == '__main__':
     app.run()
---
+++ static/js/main_2.js
@@ -0,0 +1,75 @@
+$(document).ready(()=>{
+
+   let mainOutput = $('#output')
+   let subOutput = $('#output2')
+   let op = $('#operator')
+   let num1 = $('#num1')
+   let num2 = $('#num2')
+   let temp = $('#temp')
+   let clearData = () =>{
+       num1.val('');
+       op.val('');
+       num2.val('');
+       temp.val('');
+   };
+   let clearOutput = () =>{
+      mainOutput.html('');
+      subOutput.html('');
+  };
+  let digitError = ()=>{
+      mainOutput.html(0);
+      subOutput.html('Reach Digit Limit');
+      temp.val(0);
+  };
+
+    $('#clearButton').click(()=>{
+      mainOutput.html('0');
+      subOutput.html('');
+      clearData();
+  });
+  $('.btn-default').click(function(){
+      if (('+-*/'.indexOf(mainOutput.html()) !=-1)){
+          mainOutput.html('')
+      }
+      if ($(this).val()=='.' && (mainOutput.html()).indexOf('.') != -1){
+          return;
+      }
+      if (mainOutput.html()=='0'
+          || subOutput.html() == 'Reach Digit Limit'){
+          clearOutput();
+      }
+      if (temp.val() !== ''){
+          clearOutput();
+          clearData();
+      }
+      mainOutput.append($(this).val());
+      subOutput.append($(this).val());
+      if (mainOutput.html().length > 12) {
+          digitError();
+      }
+  });
+
+  $('.btn-operate').click(function(){
+      let newOperator = $(this).val();
+      alert(newOperator)
+      if (num1.val() !== '' && ('+-*/').indexOf(num1.val()) == -1 && op.val() == ''){
+
+          num2.val(mainOutput.html());
+          if (('+-*/').indexOf(num2.val()) != -1) return;
+          mainOutput.append($(this).val())
+          let x = {};
+
+
+
+      }
+  })
+});
+
+
+       /*   $.getJSON($SCRIPT_ROOT+'/calc',x,d=>{
+              if (d.result.toString().length>13){
+                  digitError();
+              } else{
+                  alert()
+              }
+          });*/
